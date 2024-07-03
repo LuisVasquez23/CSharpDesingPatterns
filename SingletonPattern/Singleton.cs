7a68c4753c86b955d263fc939388dec4ccb76f19
Luis Vasquez <Vluis5457@gmail.com>
2024-07-02 19:44:31 -0600

    Added Singleton pattern

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,28 @@
+﻿namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instances = new Singleton();
+        private int numberOfInstances = 0;
+
+        // Public instances property
+        public static Singleton Instance
+        {
+            get
+            {
+                Console.WriteLine("We already have an instance now. Use it");
+                return _instances;
+            }
+        }
+
+        //Private constructor is used to prevent
+        //creation of instances with 'new' keyword outside this class
+        private Singleton() {
+            Console.WriteLine("Instantiating inside the private constructor.");
+            numberOfInstances++;
+            Console.WriteLine("Number of instances = {0}" , numberOfInstances);
+        }
+
+
+    }
+}
